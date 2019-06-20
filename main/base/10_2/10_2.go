bb1674ade41fbff44a2fa59feef49dc8630f0f29
smart <yangxuan_321@163.com>
2019-06-20 14:34:51 +0800

    Channel的学习

--- main/base/10_2/10_2.go
+++ main/base/10_2/10_2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 //规定类型 -- 返回的channel只能 存数据 指定方向
@@ -15,10 +16,27 @@ func createWorker(workId int) worker {
 	go func() {
 		for {
 			//有时候会出现 换行来不及打印出来 就 打印了 另一条 。  连在一起了 这是 典型的线程安全问题
-			fmt.Printf("workid = %d, receive=%c", workId, <-w.in)
-			fmt.Printf("\n")
+			fmt.Printf("workid = %d, receive=%c \n", workId, <-w.in)
 			// 通知外面 打印结束
 			w.done <- true
+		}
+	}()
+	return w
+}
+
+func createWorker1(workId int, wg *sync.WaitGroup) worker1 {
+	w := worker1{
+		make(chan int),
+		func() {
+			wg.Done()
+		},
+	}
+	go func() {
+		for {
+			//有时候会出现 换行来不及打印出来 就 打印了 另一条 。  连在一起了 这是 典型的线程安全问题
+			fmt.Printf("workid = %d, receive=%c \n", workId, <-w.in)
+			// 通知外面 打印结束
+			w.done()
 		}
 	}()
 	return w
@@ -27,6 +45,11 @@ func createWorker(workId int) worker {
 type worker struct {
 	in   chan int
 	done chan bool
+}
+
+type worker1 struct {
+	in   chan int
+	done func()
 }
 
 //函数式一等公民 channel也是一等公民
@@ -42,7 +65,15 @@ func chanDemo() {
 	}
 
 	for i := 0; i < 10; i++ {
+		<-workers[i].done
+	}
+
+	for i := 0; i < 10; i++ {
 		workers[i].in <- 'A' + i //发数据
+	}
+
+	for i := 0; i < 10; i++ {
+		<-workers[i].done
 	}
 
 	// 延时 5s 防止 主线程过快结束
@@ -53,6 +84,60 @@ func chanDemo() {
 
 }
 
+//函数式一等公民 channel也是一等公民
+func chanDemo1() {
+	var workers [10]worker1 // c == nil
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	//写法二
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker1(i, &wg)
+	}
+
+	for i := 0; i < 10; i++ {
+		workers[i].in <- '0' + i //发数据
+	}
+
+	wg.Wait()
+}
+
+func ccc() {
+	is := make(chan int)
+	group := sync.WaitGroup{}
+	group.Add(20)
+	go func() {
+		for {
+			fmt.Println(<-is)
+		}
+	}()
+	for i := 0; i < 20; i++ {
+		is <- i
+	}
+
+}
+
+// deadLock 因为 Channel 在发送和接受消息时 都是 会挂起  当前 线程都处于 挂起 状态。除非 另一端 转备好，否则 goroutine 都处于 挂起状态。
+func ccc1() {
+	is := make(chan int)
+	//go func() {
+	//	fmt.Println(<- is)
+	//}()
+	is <- 1
+}
+
+// 也会出现 deadlock
+func ccc2() {
+	is := make(chan int)
+	group := sync.WaitGroup{}
+	group.Add(1)
+	go func() {
+		fmt.Println(<-is)
+		group.Done()
+	}()
+	group.Wait()
+}
+
 func main() {
-	chanDemo()
+	//chanDemo1()
+	ccc2()
 }
