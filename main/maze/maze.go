39dcec843c8bee241735b44b4885435d4fc02c8d
smart <yangxuan_321@163.com>
2019-07-04 22:05:47 +0800

    Channel的学习

--- main/maze/maze.go
+++ main/maze/maze.go
@@ -40,6 +40,8 @@ func (curr point) step(dir point) point {
 	return point{curr.i + dir.i, curr.j + dir.j}
 }
 
+// 两个 返回值	1.返回的是 给定 点坐标的 作为横纵坐标 数组对应的 值
+//			   	2.返回的是 这个点 是否 是一个 合法的点。
 func (p point) at(grid [][]int) (int, bool) {
 	if p.i < 0 || p.i >= len(grid) {
 		return 0, false
@@ -76,11 +78,35 @@ func walkMaze(maze [][]int, start point, end point) {
 
 		for _, dir := range rundirs {
 			next := cur.step(dir)
-			q.push(next)
+
+			// 准备尝试去探索下一个坐标
+			// 那么 下一个 坐标 是否能被探索， 有两个重要的 条件：
+			// 1.也就是说 下一个 点 对应的 强的 位置 必须能走。不能为1 （因为 1-不能走，0-能走）
+			// 且也不能为start
+			//		maze as next is 0 and next != start
+			// 2.也就是说 下一个 点 的 下一步 ，  只能为 初始化的0 的。否则 就代表 有人走过
+			// 		steps at next is 0
+			// 只有条件全满足 才能 继续 走下一步
 
 			value, ok := next.at(maze)
-		}
+			// 迷宫的下一个 位置 不能越界。且 值 也不能为1 （为1 就撞墙了 ， 不能被通过）
+			if (!ok) || value == 1 {
+				continue
+			}
 
+			// 不能是 起始点
+			if next.i == start.i || next.j == start.j {
+				continue
+			}
+
+			// 存储 走过 路径 （走的每一步 记录 的 数组）， 与迷宫 对应相同位置的地方 也不能 大于 0。大于0 代表 有人走过
+			value1, ok1 := next.at(steps)
+			if (!ok1) || value1 > 0 {
+				continue
+			}
+
+			q.push(next)
+		}
 	}
 }
 
